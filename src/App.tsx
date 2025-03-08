7c6550fc5ab3f13c545a5c51a4efd5c9ce04db00
haifeng <haifeng@byai.com>
2025-03-08 11:33:11 +0800

    fix: error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { FC, ChangeEvent, useState } from "react";
-import "./App.css";
-import TodoTask from "./Components/TodoTask";
-import { ITask } from "./Interfaces";
+import React, { FC, ChangeEvent, useState } from 'react';
+import './App.css';
+import TodoTask from './Components/TodoTask';
+import { ITask } from './Interfaces';
 
 const App: FC = () => {
-  const [task, setTask] = useState<string>("");
+  const [task, setTask] = useState<string>('');
   const [deadline, setDealine] = useState<number>(0);
   const [todoList, setTodoList] = useState<ITask[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.name === "task") {
+    if (event.target.name === 'task') {
       setTask(event.target.value);
     } else {
       setDealine(Number(event.target.value));
@@ -19,40 +19,34 @@ const App: FC = () => {
   const addTask = (): void => {
     const newTask = { taskName: task, deadline: deadline };
     setTodoList([...todoList, newTask]);
-    setTask("");
+    setTask('');
     setDealine(0);
   };
 
   const completeTask = (taskNameToDelete: string): void => {
     setTodoList(
       todoList.filter((task) => {
-        return task.taskName != taskNameToDelete;
+        return task.taskName !== taskNameToDelete;
       })
     );
   };
 
   return (
-    <div className="App">
-      <div className="header">
-        <div className="inputContainer">
+    <div className='App'>
+      <div className='header'>
+        <div className='inputContainer'>
+          <input type='text' placeholder='Task...' name='task' value={task} onChange={handleChange} />
           <input
-            type="text"
-            placeholder="Task..."
-            name="task"
-            value={task}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            placeholder="Deadline (in Days)..."
-            name="deadline"
+            type='number'
+            placeholder='Deadline (in Days)...'
+            name='deadline'
             value={deadline}
             onChange={handleChange}
           />
         </div>
         <button onClick={addTask}>Add Task</button>
       </div>
-      <div className="todoList">
+      <div className='todoList'>
         {todoList.map((task: ITask, key: number) => {
           return <TodoTask key={key} task={task} completeTask={completeTask} />;
         })}
